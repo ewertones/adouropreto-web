26aa1fd458fd13c3dbf2832b4404f5b1601d97f9
Ewerton <ewertonesouza@gmail.com>
2024-04-11 21:15:40 -0300

    Updated meta and /churrasco page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,16 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "AD Ouro Preto",
+    description: "Site não oficial da Assembleia de Deus no bairro Ouro Preto.",
+    icons: {
+        icon: "/favicon.ico",
+        shortcut: "/favicon.ico",
+        apple: "/favicon.ico",
+        other: {
+            rel: "/favicon.ico",
+            url: "/favicon.ico",
+        },
+    },
 };
 
 export default function RootLayout({
@@ -15,7 +25,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="pt-br">
-            <title>AD Ouro Preto</title>
             <body className={inter.className}>{children}</body>
         </html>
     );
