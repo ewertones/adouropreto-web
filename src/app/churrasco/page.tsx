70403f6f399e161fe56fb24741018f2b4ccd908e
Ewerton <ewertonesouza@gmail.com>
2024-05-03 10:58:29 -0300

    Fixed url

--- src/app/churrasco/page.tsx
+++ src/app/churrasco/page.tsx
@@ -1,51 +1,8 @@
 import React from "react";
-import styles from "./styles.module.css";
-import WhatsappButton from "./WhatsappButton";
 import { redirect } from "next/navigation";
 
 const BarbecuePage: React.FC = () => {
-    redirect("https://lets.events/e/churrascao-do-ama/");
-    // return (
-    //     <div className={styles.churrascoBackground}>
-    //         <div className={styles.churrascoBody}>
-    //             <div className={styles.container}>
-    //                 {/* <div className={`${styles.imageContainer} ${styles.overlay}`}>
-    //                 <img src="/churrasco.webp" alt="Churrasco" className={styles.image} />
-    //             </div> */}
-    //                 <h1 className={styles.title}>Churrascão - Juventude&nbsp;AMA</h1>
-    //                 <div className={styles.info}>
-    //                     <p>
-    //                         <strong>Data:</strong> 26 de Maio
-    //                     </p>
-    //                     <p>
-    //                         <strong>Horário:</strong> 12:00 em diante
-    //                     </p>
-    //                     <p>
-    //                         <strong>Valores:</strong>
-    //                         <ul className="mt-1">
-    //                             <li>Churrasco completo: R$22,00</li>
-    //                             <li>Churrasco puro: R$32,40</li>
-    //                             <li>Tábua mista: R$27,00</li>
-    //                         </ul>
-    //                     </p>
-    //                 </div>
-    //                 <hr className={styles.horizontalLine} />
-    //                 <div className={styles.additionalInfo}>
-    //                     <p>Junte-se a nós e participe desse grande evento!</p>
-    //                     <p>O que vai ter?</p>
-    //                     <ul>
-    //                         <li>Muita carne!</li>
-    //                         <li>Música ao vivo</li>
-    //                         <li>Brincadeiras</li>
-    //                         <li>E muito mais!</li>
-    //                     </ul>
-    //                     <p className="mt-5">Não perca essa oportunidade e garanta já seu ingresso!</p>
-    //                 </div>
-    //                 <WhatsappButton />
-    //             </div>
-    //         </div>
-    //     </div>
-    // );
+    redirect("https://lets.events/e/churrascao-do-ama-2/");
 };
 
 export default BarbecuePage;
